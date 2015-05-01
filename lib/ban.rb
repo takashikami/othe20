dbd1421674eead22fbd8a773a2fa6c5d051b45c8
Takashi KAMI <takashi.kami@seiko-sol.co.jp>
2015-05-01 18:29:08 +0900

    still still work in progress

--- lib/ban.rb
+++ lib/ban.rb
@@ -2,100 +2,80 @@ SIRO=2
 KURO=3
 
 class Mas
-  attr_reader :x,:y,:c
+  attr_accessor :x,:y,:c
   def inspect
     [@x,@y]
   end
-  def initialize(x,y)
+  def initialize(x,y,b)
+    @b = b
     @x = x
     @y = y
     @c = 0
-    if x<0 || y<0
-      self
-    else
-      nil
-    end
   end
   def adr
     @y*8+@x
   end
-  def self.adr(a)
+  def self.pos(x,y,b)
+    return if x<0 || y<0
+    Mas.new(x,y,b)
+  end
+  def self.adr(a,b)
     x=a%8
     y=a/8
-    Mas.new(x,y)
+    Mas.new(x,y,b)
   end
   def around
+    @b.m.map(&:c)
     [
-        Mas.new(@x+1,@y+1),
-        Mas.new(@x+1,@y),
-        Mas.new(@x+1,@y-1),
-        Mas.new(@x,  @y+1),
-        Mas.new(@x,  @y-1),
-        Mas.new(@x-1,@y+1),
-        Mas.new(@x-1,@y),
-        Mas.new(@x-1,@y-1),
-    ]
+        @b.mget(@x+1,@y+1),
+        @b.mget(@x+1,@y),
+        @b.mget(@x+1,@y-1),
+        @b.mget(@x,  @y+1),
+        @b.mget(@x,  @y-1),
+        @b.mget(@x-1,@y+1),
+        @b.mget(@x-1,@y),
+        @b.mget(@x-1,@y-1),
+    ].select{|m|m}
+  end
+  def vector(p)
+    [p.x-@x,p.y-@y]
+  end
+  def vget(v,n)
+
   end
 end
 
 class Ban
-  def initialize(azbz)
-    az,bz = azbz.unpack('a8a8')
-    a0 = az.unpack('B8'*8).map{|x|x.chars.map(&:to_i)}
-    b0 = bz.unpack('B8'*8).map{|x|x.chars.map(&:to_i)}
-    @b = a0.zip(b0).map{|a1,b1|a1.zip(b1).map{|a2,b2|a2|(b2*2)}}
+  attr_accessor :m
+  def initialize
+    @m = (0..63).to_a.map{|a|Mas.adr(a,self)}
   end
 
   def print
-    @b.each{|r|p r}
+    @m.map(&:c).map(&:to_s).join.unpack('a8'*8).each{|r|p r}
     nil
   end
 
+  def mget(x,y)
+    @m[x+8*y] unless x<0 || y<0
+  end
   def pset(x,y,c)
-    @b[x][y]=c
+    @m[x+8*y].c=c
   end
 
-  def searchxxx(c)
-    ps = []
-    8.times do |x|
-      8.times do |y|
-        ps << [x,y] if @b[x][y] == c
-      end
-    end
-    ps
-  end
   def search(c)
-    ps = []
-    8.times do |x|
-      8.times do |y|
-        ps << Mas.new(x,y) if @b[x][y] == c
-      end
-    end
-    ps
-  end
-
-  def around(x,y)
-    [
-        [x+1,y+1],
-        [x+1,y],
-        [x+1,y-1],
-        [x,  y+1],
-        [x,  y-1],
-        [x-1,y+1],
-        [x-1,y],
-        [x-1,y-1]
-    ]
+    @m.select{|m|m.c == c}
   end
 
   def taketurn(ps)
-    ps.map(&:around).flatten.map(&:adr).uniq.map{|a|Mas.adr(a)}
+    around = ps.map(&:around).flatten.uniq
   end
 
 end
 
 
 z = [''].pack('a16')
-ban = Ban.new(z)
+ban = Ban.new
 ban.pset(3,3,KURO)
 ban.pset(4,4,KURO)
 ban.pset(4,3,SIRO)
@@ -103,8 +83,36 @@ ban.pset(3,4,SIRO)
 ban.print
 puts
 
-nx = ban.search(SIRO)
-p nx
-p ban.taketurn(nx)
-
-
+tern = KURO
+nx = ban.search(SIRO).map(&:around).flatten.uniq.select{|m|m.c==0}
+nx.map do |nxx|
+  nn = nxx.around.select{|mx|mx.c==SIRO}
+  nn.each do |nnn|
+    dx,dy = nnn.vector(nxx)
+    rev = [nnn]
+    (2..8).each do |i|
+      reva = ban.mget(nxx.x+dx,nxx.y+dy)
+      p ["reva",reva,reva.c]
+      case reva.c
+        when SIRO
+          rev << reva
+          p "#{i} SIRO"
+        when KURO
+          p "#{i} GET"
+          break
+        when 0
+          p "#{i} SUKA"
+          rev = []
+          break
+        when nil
+          p "#{i} KABE"
+          rev = []
+          break
+      end
+    end
+    p ["rev",rev]
+  end
+  p [nxx,nn]
+end
+#ban.taketurn(nx)
+#p ban.mget(0,0).around
