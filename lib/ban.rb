85e889bc8e262240527c045e33b8bf8cbea38a12
Takashi KAMI <takashi.kami@seiko-sol.co.jp>
2015-05-01 19:31:32 +0900

    GET ok good output

--- lib/ban.rb
+++ lib/ban.rb
@@ -87,37 +87,36 @@ ban.print
 puts
 
 tern = KURO
-nx = ban.search(SIRO).map(&:around).flatten.uniq.select{|m|m.c==0}
-nx.map do |nxx|
-  nn = nxx.around.select{|mx|mx.c==SIRO}
-  nn.each do |nnn|
-    #p ["========#{nnn}"]
-    dx,dy = nxx.vector(nnn)
-    #p ["vector",nnn,nxx,dx,dy]
-    rev = [nnn]
+nxs = ban.search(SIRO).map(&:around).flatten.uniq.select{|m|m.c==0}
+nxcans = nxs.map do |nx|
+  nxarounds = nx.around.select{|mx|mx.c==SIRO}
+  revs = []
+  nxarounds.each do |nxa|
+    #dx,dy = nx.vector(nxa)
+    dx,dy = [nxa.x-nx.x, nxa.y-nx.y]
+    rev = [nxa]
     (2..8).each do |i|
-      reva = ban.mget(nxx.x+dx*i,nxx.y+dy*i)
-      #p ["reva",reva,reva.c]
+      reva = ban.mget(nx.x+dx*i,nx.y+dy*i)
       case reva.c
         when SIRO
-          #p "#{i} SIRO"
           rev << reva
         when KURO
-          p "#{i} GET #{nxx.inspect} #{rev.map(&:inspect)} #{reva.inspect}"
+          #p "#{i} GET #{nx.inspect} #{rev.map(&:inspect)} #{reva.inspect}"
           break
         when 0
-          #p "#{i} SUKA"
           rev = []
           break
         when nil
-          #p "#{i} KABE"
           rev = []
           break
       end
     end
-    #p ["rev",rev]
+    revs.concat(rev)
   end
-  #p [nxx,nn]
+  [nx, revs.flatten] unless revs.flatten.empty?
+  #{nx => revs.flatten}
 end
+nxcans.select{|nx|nx}.each{|nx|p [nx[1].size, nx]}
+
 #ban.taketurn(nx)
 #p ban.mget(0,0).around
