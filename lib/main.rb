f4898df43d8d6523c2cb42ef8d64a8448911caa6
Takashi KAMI <takashi.kami@seiko-sol.co.jp>
2015-06-05 12:52:27 +0900

    extend bandb

--- lib/main.rb
+++ lib/main.rb
@@ -6,7 +6,23 @@ include Col
 #
 require 'active_record'
 
-#create table bandb(id int auto_increment, dump char(34) unique not null, index(id));
+=begin
+create table bandb(
+  id bigint auto_increment primary key,
+  olddump char(34) not null,
+  dump char(34) unique not null,
+  stat smallint not null default 0,
+  turn smallint,
+  placex smallint,
+  placey smallint,
+  count0 smallint,
+  count2 smallint,
+  count3 smallint,
+  updated_on timestamp,
+  created_on timestamp,
+  index(olddump)
+);
+=end
 ActiveRecord::Base.establish_connection(
     host:     'localhost',
     database: 'othe20',
@@ -44,6 +60,13 @@ loop do
       p [n.olddump, n.wait, n.place, n.dump, n.counts]
       db = BanDB.new
       db.dump = n.dump
+      db.olddump = n.olddump
+      db.turn = n.wait
+      db.placex = n.place[0]
+      db.placey = n.place[1]
+      db.count0 = n.counts[0]
+      db.count2 = n.counts[2]
+      db.count3 = n.counts[3]
       db.save
     end
   end
